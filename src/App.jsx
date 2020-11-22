1d21133c8a25bbbc3e682ab602a300abec1ce55d
Raj Sharma <xrehpicx@gmail.com>
2020-11-23 00:22:15 +0530

    voting-works

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import contract from "@truffle/contract";
 import ElectionContract from "./contracts/Election.json";
@@ -8,32 +8,65 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  const [candidates, setCandidates] = useState([]);
+  const [account, setAccount] = useState();
+  const [instance, setInstance] = useState();
+
   useEffect(() => {
     getWeb3().then(async (web3) => {
       const Election = contract(ElectionContract);
       console.log(web3.currentProvider);
       Election.setProvider(web3.currentProvider);
-      const instance = await Election.deployed();
-      const candidateCount = await instance.candidatesCount();
-      console.log(candidateCount.toNumber());
+      const ins = await Election.deployed();
+      setInstance(ins);
+      const candidateCount = (await ins.candidatesCount()).toNumber();
+
+      for (let i = 1; i <= candidateCount; i++) {
+        const candidate = await ins.candidates(i);
+        const candidateId = candidate[0].toNumber();
+        const candidateName = candidate[1];
+        const candidateVotes = candidate[2].toNumber();
+        // console.log(candidateId, candidateName, candidateVotes);
+        const chainId = await web3.eth.getChainId();
+        console.log(chainId);
+        web3.eth.getCoinbase(function (err, acc) {
+          if (err === null) {
+            setAccount(acc);
+          }
+        });
+        setCandidates((cans) => [
+          ...cans,
+          { id: candidateId, name: candidateName, votes: candidateVotes },
+        ]);
+      }
     });
   }, []);
+
+  const onclick = (candidateId) => {
+    instance.vote(candidateId, { from: account });
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        your account: {account}
+        {candidates.map((can, i) => (
+          <div
+            onClick={() => onclick(can.id)}
+            key={i}
+            style={{
+              padding: "10px",
+              margin: "10px",
+              background: "black",
+            }}
+          >
+            <p>{can.name}</p>
+            <div>
+              <p>id: {can.id}</p>
+              <p>votes: {can.votes}</p>
+            </div>
+          </div>
+        ))}
       </header>
     </div>
   );
